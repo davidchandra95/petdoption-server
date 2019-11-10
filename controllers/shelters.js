8391bc86ed169bb4bc83753103c7e788074a5a63
davidchandra95 <davidchandra95@yahoo.com>
2019-11-10 13:49:57 +0700

    add shelter's crud functionality

--- controllers/shelters.js
+++ controllers/shelters.js
@@ -1,38 +1,81 @@
-exports.fetchAll = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, data: { message: 'show all shelters' } });
+const Shelter = require('../models/Shelter');
+const HttpStatus = require('http-status-codes');
+
+exports.fetchAll = async (req, res, next) => {
+  try {
+    const shelters = await Shelter.find();
+    res
+      .status(HttpStatus.OK)
+      .json({ success: true, data: { shelters, count: shelters.length } });
+  } catch (err) {
+    res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ success: false, message_error: err.message });
+  }
 };
 
-exports.getByID = (req, res, next) => {
-  res
-    .status(200)
-    .json({
-      success: true,
-      data: { message: `Get shelter ${req.params.id}` }
+exports.getByID = async (req, res, next) => {
+  try {
+    const shelter = await Shelter.findById(req.params.id);
+
+    if (!shelter) {
+      return res
+        .status(HttpStatus.OK)
+        .json({ success: false, message_error: 'shelter not found.' });
+    }
+    res.status(HttpStatus.OK).json({ success: true, data: { shelter } });
+  } catch (err) {
+    res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ success: false, message_error: err.message });
+  }
+};
+
+exports.save = async (req, res, next) => {
+  try {
+    const shelter = await Shelter.create(req.body);
+
+    res.status(HttpStatus.OK).json({ success: true, data: { shelter } });
+  } catch (err) {
+    res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ success: false, message_error: err.message });
+  }
+};
+
+exports.update = async (req, res, next) => {
+  try {
+    const shelter = await Shelter.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
     });
+
+    if (!shelter) {
+      return res
+        .status(HttpStatus.OK)
+        .json({ success: false, message_error: `shelter doesn't exists.` });
+    }
+    res.status(HttpStatus.OK).json({ success: true, data: { shelter } });
+  } catch (err) {
+    res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ success: false, message_error: err.message });
+  }
 };
 
-exports.save = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, data: { message: 'Create new shelter' } });
-};
+exports.remove = async (req, res, next) => {
+  try {
+    const shelter = await Shelter.findByIdAndDelete(req.params.id);
 
-exports.update = (req, res, next) => {
-  res
-    .status(200)
-    .json({
-      success: true,
-      data: { message: `Update shelter ${req.params.id}` }
-    });
-};
-
-exports.remove = (req, res, next) => {
-  res
-    .status(200)
-    .json({
-      success: true,
-      data: { message: `Delete ${req.params.id} shelter` }
-    });
+    if (!shelter) {
+      return res
+        .status(HttpStatus.OK)
+        .json({ success: false, message_error: `shelter doesn't exists.` });
+    }
+    res.status(HttpStatus.OK).json({ success: true, data: {} });
+  } catch (err) {
+    res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ success: false, message_error: err.message });
+  }
 };
