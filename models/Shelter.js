04f238e981ae2f919d5747fdcd7ffd5273f8627a
davidchandra95 <davidchandra95@yahoo.com>
2019-11-10 12:39:22 +0700

    add mongo connection and shelter model

---
+++ models/Shelter.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+
+const ShelterSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'Name can not be more than 50 characters']
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Description is required'],
+    maxlength: [200, 'Description can not be more than 200 characters']
+  },
+  website: {
+    type: String,
+    match: [
+      /https?\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      'Please use a valid URL with HTTP or HTTPS'
+    ]
+  },
+  phone: {
+    type: String,
+    maxlength: [20, 'Phone number can not be longer than 20 characters']
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+      'Please add a valid email'
+    ]
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address']
+  },
+  location: {
+    // GeoJSON point
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+  },
+  animals: {
+    type: [String],
+    required: true,
+    enum: ['Dog', 'Cat', 'Other']
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be atleast 1'],
+    max: [10, 'Rating can not be more than 10']
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Shelter', ShelterSchema);
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //Route files
 const shelterRoutes = require('./routes/shelters');
@@ -7,9 +10,14 @@ const shelterRoutes = require('./routes/shelters');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
 const app = express();
 
-app.use('/api/v1/bootcamps', shelterRoutes);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use('/api/v1/shelters', shelterRoutes);
 
 app.get('/', (req, res) => {
   res
@@ -18,7 +26,16 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
