acffe4b01ab14db2f0a98e2703165851530591da
davidchandra95 <davidchandra95@yahoo.com>
2019-11-16 23:03:26 +0700

    add asyncHandler middleware

--- controllers/shelters.js
+++ controllers/shelters.js
@@ -1,81 +1,62 @@
 const Shelter = require('../models/Shelter');
 const HttpStatus = require('http-status-codes');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middlewares/async');
 
-exports.fetchAll = async (req, res, next) => {
-  try {
-    const shelters = await Shelter.find();
-    res
-      .status(HttpStatus.OK)
-      .json({ success: true, data: { shelters, count: shelters.length } });
-  } catch (err) {
-    return next(err);
+exports.fetchAll = asyncHandler(async (req, res, next) => {
+  const shelters = await Shelter.find();
+  res
+    .status(HttpStatus.OK)
+    .json({ success: true, data: { shelters, count: shelters.length } });
+});
+
+exports.getByID = asyncHandler(async (req, res, next) => {
+  const shelter = await Shelter.findById(req.params.id);
+
+  if (!shelter) {
+    return next(
+      new ErrorResponse(
+        `shelter with ${req.params.id} id doesn't exists`,
+        HttpStatus.NOT_FOUND
+      )
+    );
   }
-};
+  res.status(HttpStatus.OK).json({ success: true, data: { shelter } });
+});
 
-exports.getByID = async (req, res, next) => {
-  try {
-    const shelter = await Shelter.findById(req.params.id);
+exports.save = asyncHandler(async (req, res, next) => {
+  const shelter = await Shelter.create(req.body);
 
-    if (!shelter) {
-      return next(
-        new ErrorResponse(
-          `shelter with ${req.params.id} id doesn't exists`,
-          HttpStatus.NOT_FOUND
-        )
-      );
-    }
-    res.status(HttpStatus.OK).json({ success: true, data: { shelter } });
-  } catch (err) {
-    next(err);
+  res.status(HttpStatus.OK).json({ success: true, data: { shelter } });
+});
+
+exports.update = asyncHandler(async (req, res, next) => {
+  const shelter = await Shelter.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  if (!shelter) {
+    return next(
+      new ErrorResponse(
+        `shelter with ${req.params.id} id doesn't exists`,
+        HttpStatus.NOT_FOUND
+      )
+    );
   }
-};
+  res.status(HttpStatus.OK).json({ success: true, data: { shelter } });
+});
 
-exports.save = async (req, res, next) => {
-  try {
-    const shelter = await Shelter.create(req.body);
+exports.remove = asyncHandler(async (req, res, next) => {
+  const shelter = await Shelter.findByIdAndDelete(req.params.id);
 
-    res.status(HttpStatus.OK).json({ success: true, data: { shelter } });
-  } catch (err) {
-    return next(err);
+  if (!shelter) {
+    return next(
+      new ErrorResponse(
+        `shelter with ${req.params.id} id doesn't exists`,
+        HttpStatus.NOT_FOUND
+      )
+    );
   }
-};
-
-exports.update = async (req, res, next) => {
-  try {
-    const shelter = await Shelter.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-
-    if (!shelter) {
-      return next(
-        new ErrorResponse(
-          `shelter with ${req.params.id} id doesn't exists`,
-          HttpStatus.NOT_FOUND
-        )
-      );
-    }
-    res.status(HttpStatus.OK).json({ success: true, data: { shelter } });
-  } catch (err) {
-    return next(err);
-  }
-};
-
-exports.remove = async (req, res, next) => {
-  try {
-    const shelter = await Shelter.findByIdAndDelete(req.params.id);
-
-    if (!shelter) {
-      return next(
-        new ErrorResponse(
-          `shelter with ${req.params.id} id doesn't exists`,
-          HttpStatus.NOT_FOUND
-        )
-      );
-    }
-    res.status(HttpStatus.OK).json({ success: true, data: {} });
-  } catch (err) {
-    return next(err);
-  }
-};
+  res.status(HttpStatus.OK).json({ success: true, data: {} });
+});
